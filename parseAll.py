aa8f268c5ae30064c1527d8fe3fac6214435fa50
CJ5518 <carsonrueber@gmail.com>
2023-09-19 14:59:50 -0700

    Basic implementation

--- parseAll.py
+++ parseAll.py
@@ -1,6 +1,7 @@
 import os
 import signal
 from slippi import Game
+from slippi.event import StateFlags
 
 #Script assumes the following file structure:
 #./
@@ -9,16 +10,45 @@ from slippi import Game
 #./SlippiFiles/Job_2222/Game_##.slp
 #./SlippiFiles/Job_3333/Game_##.slp
 
+
+#matchups[p0][p1] = Number of times char0 (p0) beat char1 (p1)
+#This also means matchups[p1][p0] is number of times char0 lost to char1
+#Number of games played between the two chars is the two above added.
+matchups = {}
+
+#Returns 0 if the player in port 0 won, and 1 otherwise
+def getWinner(game):
+    port0 = game.frames[-1].ports[0].leader.post.flags & StateFlags.DEAD
+    if port0 == StateFlags.DEAD:
+        return 0
+    else:
+        return 1
+
 #Do something with a .slp file
 def parseGame(path):
     try:
-        game = Game(path, skip_frames=True)
-        numChars = len(game.metadata.players[0].characters)
-        if numChars != 1:
-            print(numChars)
-            print("Parsed game " + path)
-    except:
+        game = Game(path)
+        p0 = int(game.start.players[0].character)
+        p1 = int(game.start.players[1].character)
+        winnerPort = getWinner(game)
+
+        #Make sure the structure of the dict is ready
+        if p0 not in matchups:
+            matchups[p0] = {}
+        if p1 not in matchups[p0]:
+            matchups[p0][p1] = 0
+        if p1 not in matchups:
+            matchups[p1] = {}
+        if p0 not in matchups[p1]:
+            matchups[p1][p0] = 0
+
+        if getWinner(game) == 0:
+            matchups[p0][p1] += 1
+        else:
+            matchups[p1][p0] += 1
+    except Exception as e:
         print("Couldn't parse replay " + path)
+        print(e)
 
 
 def signal_handler(sig, frame):
@@ -37,3 +67,5 @@ for root, dirs, files in os.walk("./SlippiFiles/"):
     for dire in dirs:
         handleDir(root + dire)
     break
+
+print(matchups)
