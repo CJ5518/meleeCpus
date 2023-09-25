6588762fb4ee1484107c5b8ada84be6c8c23b3f9
CJ5518 <carsonrueber@gmail.com>
2023-09-25 12:09:46 -0700

    First stab at consolidation

--- parseConsolidate.lua
+++ parseConsolidate.lua
@@ -6,14 +6,35 @@ local function decodeFile(path)
     return json.decode(file:read("a"))
 end
 
+local master = {};
+
+local function consolidate(obj)
+    for i,v in pairs(obj) do
+        if master[i] then
+            for i2,v2 in pairs(v) do
+                if master[i][i2] then
+                    master[i][i2] = master[i][i2] + v2
+                else
+                    master[i][i2] = v2
+                end
+            end
+        else
+            master[i] = {}
+            for i2, v2 in pairs(v) do
+                master[i][i2] = v2
+            end
+        end
+    end
+end
 
 local function forAllFiles(root)
     for file in lfs.dir(root) do
         if file ~= "." and file ~= ".." then
             local obj = decodeFile(file)
-            print(obj)
+            consolidate(obj)
         end
     end
+    print(json.encode(master))
 end
 
 forAllFiles("./parseOuts/")
