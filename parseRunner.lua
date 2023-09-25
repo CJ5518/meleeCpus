c3a63de46fa994da917a1675ea93d604d9ade466
CJ5518 <carsonrueber@gmail.com>
2023-09-25 11:33:52 -0700

    Most likely working parse runner

--- parseRunner.lua
+++ parseRunner.lua
@@ -4,7 +4,7 @@
 local function runScript(folderName)
 	local command = string.format("printf '#!/bin/bash\\n\\n"  ..
 	"#SBATCH -p tiny\\n\\n" ..
-	"apptainer exec --writable -f ../pythonContainer/ python3 /root/meleeCpus/parseFolder.py -i /root/meleeCpus/Slippi Files/%s > /root/meleeCpus/parseOuts/%s.json",
+	"apptainer exec --writable -f ../pythonContainer/ python3 /root/meleeCpus/parseFolder.py -i /root/meleeCpus/SlippiFiles/%s > ~/meleeCpus/parseOuts/%s.json'",
 	folderName,folderName
 )
 	os.execute(command .. " | sbatch")
