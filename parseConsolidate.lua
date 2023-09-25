005a5a38a6be5d8e91745dda65aa4c3d0de6c240
CJ5518 <carsonrueber@gmail.com>
2023-09-25 12:00:34 -0700

    Fix parser bug

--- parseConsolidate.lua
+++ parseConsolidate.lua
@@ -2,15 +2,17 @@ local json = require("json")
 local lfs = require("lfs")
 
 local function decodeFile(path)
-    local file = io.open(path,"r")
+    local file = io.open("./parseOuts/" .. path,"r")
     return json.decode(file:read("a"))
 end
 
 
 local function forAllFiles(root)
     for file in lfs.dir(root) do
-        local obj = decodeFile(file)
-        print(obj)
+        if file ~= "." and file ~= ".." then
+            local obj = decodeFile(file)
+            print(obj)
+        end
     end
 end
 
